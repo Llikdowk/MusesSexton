8d256f1523d3097fd01d8d1bb4a3b9a1eb7cd6af
Guillermo Ferrando <guillermofm7@gmail.com>
2016-05-11 12:39:17 +0200

    terrain textures by hightmap

---
+++ Assets/CustomAssets/Scripts/DigBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.CustomAssets.Scripts {
+    public class DigBehaviour : MonoBehaviour {
+        public Terrain terrain;
+        private TerrainData terrainData;
+
+        public void Awake() {
+            terrainData = terrain.terrainData;
+        }
+
+        public void Start () {
+	
+        }
+	
+        public void Update () {
+	
+        }
+    }
+}
