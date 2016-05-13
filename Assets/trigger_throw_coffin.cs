a4af25ad494a58a146c9a79cb334397513d14480
Guillermo Ferrando <guillermofm7@gmail.com>
2016-05-13 17:20:40 +0200

    some tween engine added but not used, AnimationCurve used instead

---
+++ Assets/trigger_throw_coffin.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.CustomAssets.Scripts.CustomInput;
+
+public class trigger_throw_coffin : MonoBehaviour {
+    public Transform node1;
+    public Transform node2;
+    public AnimationCurve curve;
+    private Transform original;
+    private Transform coffin;
+	
+	public void Start () {
+        coffin = GameObject.Find("Coffin_debug").transform;
+        original = new GameObject().transform;
+	}
+	
+	
+    public void OnTriggerEnter(Collider c) {
+        Debug.Log("hasEntered!");
+    }
+
+	public void OnTriggerStay (Collider c) {
+        //Debug.Log("stay!");
+	    if (GameActions.checkAction(Action.USE, Input.GetKeyDown)) {
+            //doAction(Time.time);
+            original.position = coffin.position;
+            original.rotation = coffin.rotation;
+            StartCoroutine(doAction());
+	    }
+	}
+
+    
+    private IEnumerator doAction() {
+        float t = 0;
+        coffin.parent = null;
+        while (t < 1f) {
+            float c = curve.Evaluate(t);
+            coffin.position = Vector3.Slerp(original.position, node1.position, t) + new Vector3(0, c, 0);
+            coffin.rotation = Quaternion.Slerp(original.rotation, node1.rotation, t);
+            t += .016f;
+            yield return new WaitForSeconds(.016f);
+        }
+    }
+    
+}
