253b1f3e94063967476baffe29c91452df97faf7
Guillermo Ferrando <guillermofm7@gmail.com>
2016-05-16 12:37:34 +0200

    loading system done

--- Assets/CustomAssets/Scripts/Player/PlayerController.cs
+++ Assets/CustomAssets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,13 @@
 ﻿
 using Assets.CustomAssets.Scripts.CustomInput;
 using Assets.CustomAssets.Scripts.Player.Behaviour;
+using Cubiquity;
 using UnityEngine;
 
 namespace Assets.CustomAssets.Scripts.Player {
     public class PlayerController : MonoBehaviour {
         private Player player;
+        public TerrainVolume terrainVolume;
 
         public void Start() {
             player = Player.getInstance();
---
+++ Assets/wait_till_load.cs
@@ -0,0 +1,25 @@
+﻿using Assets;
+using Assets.CustomAssets.Scripts.Player;
+using Cubiquity;
+using UnityEngine;
+
+public class wait_till_load : MonoBehaviour {
+
+    public fade UICurtain;
+    public TerrainVolume terrain;  
+    private PlayerController controller;
+    private bool alreadyLoaded = false;
+
+	public void Start () {
+        UICurtain.gameObject.SetActive(true);
+        controller = gameObject.GetComponent<PlayerController>();
+	}
+	
+	public void Update () {
+	    if (!alreadyLoaded && terrain.isMeshSyncronized) {
+            controller.enabled = true;
+            alreadyLoaded = true;
+            UICurtain.fadeOut();
+	    }
+	}
+}
