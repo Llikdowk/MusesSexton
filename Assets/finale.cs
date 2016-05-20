b17d14cc62c241d12f575347e6fa46571eeb668e
Guillermo Ferrando <guillermofm7@gmail.com>
2016-05-20 04:24:22 +0200

    landmarks added as prefabs

---
+++ Assets/finale.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finale : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider c) {
+        Debug.Log("has entered1!");
+        RenderSettings.fogDensity = 1.0f;
+    }
+}
